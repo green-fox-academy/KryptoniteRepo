523e242cc70d89ba21a38d6b0d70473afd75b2ea
bulint <bulint0324@gmail.com>
2018-02-13 15:35:22 +0100

    feat(b5): modified count method

--- KryptoniteProject/KryptoniteProject/Counter.cs
+++ KryptoniteProject/KryptoniteProject/Counter.cs
@@ -6,9 +6,9 @@ namespace KryptoniteProject
 {
     class Counter
     {
-        public void Count(int age)
+        public void Count(int age, string dog)
         {
-            Console.WriteLine(age);
+            Console.WriteLine(age + dog);
         }
     }
 }
