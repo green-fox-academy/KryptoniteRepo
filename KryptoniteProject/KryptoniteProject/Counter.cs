9f19519b56f1aed8e4d14d3eaa7a707c4316356c
bulint <bulint0324@gmail.com>
2018-02-13 14:17:45 +0100

    feat(b1): created count method

---
+++ KryptoniteProject/KryptoniteProject/Counter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KryptoniteProject
+{
+    class Counter
+    {
+        public void Count()
+        {
+            Console.WriteLine("Your age");
+        }
+    }
+}
